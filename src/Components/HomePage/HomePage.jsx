919b64f11b0cce9ec5e4b88daa6c2c93411b5812
akashpal <akashpal9007@gmail.com>
2023-02-15 09:59:10 +0530

    added carousel

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import homepage from '../../assets/homepage.jpg'
 import './HomePage.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const HomePage = () => {
   return (
@@ -11,7 +13,18 @@ const HomePage = () => {
         <div style={{position: "absolute",left: "4rem"}} className='home-slog-divlast'>LIFESTYLE</div>
       </div>
       <div className='home-img-div'>
-        <img className='homepage-img' src={homepage} alt="" />
+        {/* <img className='homepage-img' src={homepage} alt="" /> */}
+        <Carousel autoPlay infiniteLoop showThumbs={false}>
+                <div>
+                    <img src={homepage} />
+                </div>
+                <div>
+                    <img src={homepage} />
+                </div>
+                <div>
+                    <img src={homepage} />
+                </div>
+            </Carousel>
       </div>
     </div>
   )
